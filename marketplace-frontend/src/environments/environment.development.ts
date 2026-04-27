e5189bb4073cddacf04390511644ba74dcc95880
aditya <adsenior0001@gmail.com>
2026-04-27 12:23:00 +0530

    fix: change gateway to port 80 to bypass firewalls

--- marketplace-frontend/src/environments/environment.development.ts
+++ marketplace-frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://52.191.217.132:8080/api/v1',
+  apiUrl: 'http://52.191.217.132/api/v1',
 };
--- marketplace-frontend/src/environments/environment.ts
+++ marketplace-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://52.191.217.132:8080/api/v1',
+  apiUrl: 'http://52.191.217.132/api/v1',
 };
